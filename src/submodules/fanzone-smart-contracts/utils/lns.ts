4e2e969262448fb0237291ec161a26c71c2b2949
mattgstevens <mattgstevens@gmail.com>
2022-02-07 11:16:10 +0000

    chore: remove submodule files
    these should not be checked into the repo, but synced from remote git repo

--- src/submodules/fanzone-smart-contracts/utils/lns.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from "ethers";
-
-export const vanityNameAsBytes32 = (vanityName: string) => {
-  return ethers.utils.formatBytes32String(vanityName);
-};
--- src/submodules/fanzone-smart-contracts/utils/types.ts
+++
@@ -1,3 +0,0 @@
-export type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-
-export type Address = string;
