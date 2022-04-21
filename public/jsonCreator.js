0b16c156021596e0720317b0ae2e2c732cd4f5be
ankitkumar9018 <ankitkumar.9018@gmail.com>
2022-04-21 17:20:03 +0200

    Update jsonCreator.js

--- public/jsonCreator.js
+++ public/jsonCreator.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-totalNumber = 555;
+totalNumber = 2;
 for (let index = 0; index <= totalNumber; index++) {
     let tempNum = Math.floor(Math.random() * 100);
     let tempString = "Gold";
@@ -7,30 +7,12 @@ for (let index = 0; index <= totalNumber; index++) {
         tempString = "Silver";
         //console.log(tempString);
     }
+
     // create a JSON object
-    let tempData = {
-        //description: "FANZONE.io - Fanzone Sports Club Pass",
-        description: "FANZONE.io - Testzone Sports Club Pass",
-        external_url: "https://fanzone.io",
-        image:
-            "ipfs://QmStQYA5jJkDnQ46mM99wrmq9FYuJibU6GnG49qPuqXBEQ",
-        animation_url: "ipfs://QmbSTe2Cdj41seYdAbdnEyQ86v1oA63SPBHCX3QJhBooEo",
-        //name: "Fanzone Sports Club ##" + index,
-        name: "Testzone Sports Club ##" + index,
-        /*attributes: [
-            {
-                trait_type: "Pass Type",
-                value: tempString,
-            },
-            {
-                trait_type: "Sale Type",
-                value: "Private",
-            },
-        ],*/
-    };
 
     if (index == 0) {
-        let tempData = {
+        console.log(index);
+        tempData = {
             //description: "FANZONE.io - Fanzone Sports Club Passes",
             description: "FANZONE.io - Testzone Sports Club Passes",
             external_link: "https://fanzone.io",
@@ -43,6 +25,31 @@ for (let index = 0; index <= totalNumber; index++) {
             fee_recipient: "0x87847d301E8Da1D7E95263c3478d7F6e229E3F4b"
         };
     }
+
+    else {
+        tempData = {
+            //description: "FANZONE.io - Fanzone Sports Club Pass",
+            description: "FANZONE.io - Testzone Sports Club Pass",
+            external_url: "https://fanzone.io",
+            image:
+                "ipfs://QmStQYA5jJkDnQ46mM99wrmq9FYuJibU6GnG49qPuqXBEQ",
+            animation_url: "ipfs://QmbSTe2Cdj41seYdAbdnEyQ86v1oA63SPBHCX3QJhBooEo",
+            //name: "Fanzone Sports Club ##" + index,
+            name: "Testzone Sports Club ##" + index,
+            /*attributes: [
+                {
+                    trait_type: "Pass Type",
+                    value: tempString,
+                },
+                {
+                    trait_type: "Sale Type",
+                    value: "Private",
+                },
+            ],*/
+        };
+    }
+
+
 
     // convert JSON object to string
     const data = JSON.stringify(tempData, null, 4);
