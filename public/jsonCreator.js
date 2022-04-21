9d821492f3247cca0c4c8015b377fe6c95195b70
ankitkumar9018 <ankitkumar.9018@gmail.com>
2022-04-21 17:14:28 +0200

    Update jsonCreator.js

--- public/jsonCreator.js
+++ public/jsonCreator.js
@@ -7,21 +7,6 @@ for (let index = 0; index <= totalNumber; index++) {
         tempString = "Silver";
         //console.log(tempString);
     }
-    if(index == 0)
-    {
-        let tempData = {
-            //description: "FANZONE.io - Fanzone Sports Club Passes",
-            description: "FANZONE.io - Testzone Sports Club Passes",
-            external_link: "https://fanzone.io",
-            image:
-                "ipfs://QmStQYA5jJkDnQ46mM99wrmq9FYuJibU6GnG49qPuqXBEQ",
-            animation_url: "ipfs://QmbSTe2Cdj41seYdAbdnEyQ86v1oA63SPBHCX3QJhBooEo",
-            //name: "Fanzone Sports Club ##",
-            name: "Testzone Sports Club",
-            seller_fee_basis_points: 500,
-            fee_recipient: "0x87847d301E8Da1D7E95263c3478d7F6e229E3F4b"
-        };
-    }
     // create a JSON object
     let tempData = {
         //description: "FANZONE.io - Fanzone Sports Club Pass",
@@ -43,6 +28,21 @@ for (let index = 0; index <= totalNumber; index++) {
             },
         ],*/
     };
+
+    if (index == 0) {
+        let tempData = {
+            //description: "FANZONE.io - Fanzone Sports Club Passes",
+            description: "FANZONE.io - Testzone Sports Club Passes",
+            external_link: "https://fanzone.io",
+            image:
+                "ipfs://QmStQYA5jJkDnQ46mM99wrmq9FYuJibU6GnG49qPuqXBEQ",
+            animation_url: "ipfs://QmbSTe2Cdj41seYdAbdnEyQ86v1oA63SPBHCX3QJhBooEo",
+            //name: "Fanzone Sports Club ##",
+            name: "Testzone Sports Club",
+            seller_fee_basis_points: 500,
+            fee_recipient: "0x87847d301E8Da1D7E95263c3478d7F6e229E3F4b"
+        };
+    }
 
     // convert JSON object to string
     const data = JSON.stringify(tempData, null, 4);
