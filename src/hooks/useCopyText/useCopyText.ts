b8bdfb5d51293ae6cdd42e189904388ab5b2853f
cre-mer <jonasmerhej1@gmail.com>
2022-04-25 16:39:26 +0200

    add canCopy to useCopyText hook

--- src/hooks/useCopyText/useCopyText.ts
+++ src/hooks/useCopyText/useCopyText.ts
@@ -3,13 +3,15 @@ import { useState } from 'react';
 export const useCopyText = () => {
   const [copied, setCopied] = useState<boolean>(false);
 
-  const copyText = () => {
-    navigator.clipboard.writeText(window.location.href);
+  const copyText = (text: string) => {
+    window.navigator.clipboard.writeText(text);
     setCopied((copied) => !copied);
     setTimeout(() => {
       setCopied((copied) => !copied);
     }, 2000);
   };
 
-  return { copied, copyText };
+  const canCopy = !!window.navigator.clipboard;
+
+  return { copied, copyText, canCopy };
 };
